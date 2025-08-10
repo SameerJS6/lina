867dd5f5c66bb9ba86311e2c407eda5d9a20263f
SameerJS6 <singh6.sameer@gmail.com>
2025-08-10 17:10:55 +0530

    style: update layout and spacing for scroll area examples in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,25 @@ import { ScrollAreaDemo as RadixScrollAreaDemo } from "@/registry/radix-ui/examp
 
 export default function Home() {
   return (
-    <main className="flex min-h-dvh flex-wrap items-center justify-center gap-4 py-6">
-      <div className="flex size-full flex-wrap justify-center gap-4">
-        <RadixScrollAreaDemo />
-        <RadixScrollAreaHorizontalDemo />
+    <main className="flex min-h-dvh flex-wrap items-center justify-center gap-8 py-6">
+      <div>
+        <div className="mx-auto max-w-2xl space-y-4">
+          <h2 className="text-3xl font-semibold tracking-tight max-sm:px-4">Radix UI</h2>
+          <div className="flex size-full flex-wrap justify-center gap-4">
+            <RadixScrollAreaDemo />
+            <RadixScrollAreaHorizontalDemo />
+          </div>
+        </div>
       </div>
 
-      <div className="flex size-full flex-wrap justify-center gap-4">
-        <BaseUIScrollAreaDemo />
-        <BaseUIScrollAreaHorizontalDemo />
+      <div>
+        <div className="mx-auto max-w-2xl space-y-4">
+          <h2 className="text-3xl font-semibold tracking-tight max-sm:px-4">Base UI</h2>
+          <div className="flex size-full flex-wrap justify-center gap-4">
+            <BaseUIScrollAreaDemo />
+            <BaseUIScrollAreaHorizontalDemo />
+          </div>
+        </div>
       </div>
     </main>
   );
