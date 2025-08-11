797ba19c0c2a86e7e81a77b22ed696dbc307d1b9
SameerJS6 <singh6.sameer@gmail.com>
2025-08-12 01:07:29 +0530

    feat: implement code highlighting functionality with support for multiple themes and line numbers

---
+++ lib/code-highlight.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import "server-only";
+
+import fs from "fs/promises";
+import path from "path";
+import type { JSX } from "react";
+import { Fragment, jsx, jsxs } from "react/jsx-runtime";
+import { toJsxRuntime } from "hast-util-to-jsx-runtime";
+import { codeToHast, type BundledLanguage } from "shiki/bundle/web";
+
+async function highlight(code: string, lang: BundledLanguage) {
+  const hast = await codeToHast(code, {
+    lang,
+    defaultColor: false,
+    themes: {
+      light: "github-light",
+      dark: "github-dark",
+    },
+    transformers: [
+      {
+        pre(node) {
+          node.properties["class"] =
+            "min-w-0 px-4 py-3.5 outline-none has-[[data-highlighted-line]]:px-0 has-[[data-line-numbers]]:px-0 has-[[data-slot=tabs]]:p-0 !bg-transparent";
+        },
+        code(node) {
+          node.properties["data-line-numbers"] = "";
+        },
+        line(node) {
+          node.properties["data-line"] = "";
+        },
+      },
+    ],
+  });
+
+  return toJsxRuntime(hast, {
+    Fragment,
+    jsx,
+    jsxs,
+  }) as JSX.Element;
+}
+
+async function getComponentCode(
+  name: string,
+  lang: BundledLanguage
+): Promise<{ code: string; highlightedCode: JSX.Element } | null> {
+  try {
+    const filePath = path.join(process.cwd(), "public", "r", `${name}.json`);
+    const fileContent = await fs.readFile(filePath, "utf-8");
+    const data = JSON.parse(fileContent);
+
+    const rawContent = data.files[0].content || "";
+    if (!rawContent.trim()) {
+      return null;
+    }
+
+    const highlightedCode = await highlight(rawContent, lang);
+
+    return { code: rawContent, highlightedCode };
+  } catch (error) {
+    console.error("Error getting component code", error);
+    return null;
+  }
+}
+
+export { highlight, getComponentCode };
